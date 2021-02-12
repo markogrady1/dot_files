e611d854c3bb6a5496490fcefa68baea03f0f8c6
Mark O Grady <mark.o'grady@broadbean.com>
2021-02-12 16:24:36 +0000

    remove clone line as already cloned; :)

--- install_env.sh
+++ install_env.sh
@@ -11,11 +11,9 @@ else
     exit
 fi
 
-git clone https://github.com/markogrady1/dot_files.git
-
-cp dot_files/perltidy/perltidyrc.symlink .perltidyrc
-cat dot_files/vim/vimrc.symlink >> .vimrc
-sudo mkdir -p ~/.vim/colors
+cp dot_files/perltidy/perltidyrc.symlink ~/.perltidyrc
+cat dot_files/vim/vimrc.symlink >> ~/.vimrc
+mkdir -p ~/.vim/colors
 cp dot_files/vim/colors/dracula.vim ~/.vim/colors/
 
 if [ ! -f "~/.bashrc" ]; then
@@ -29,8 +27,8 @@ cp dot_files/bash/aliases.symlink ~/.bash_aliases
 echo 'source ~/.bash_aliases' >> ~/.bashrc
 
 # Git configuration
-cp dot_files/git/gitconfig.symlink .gitconfig
-cp dot_files/git/githelpers.symlink .githelpers
+cp dot_files/git/gitconfig.symlink ~/.gitconfig
+cp dot_files/git/githelpers.symlink ~/.githelpers
 
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_PASSWORD
