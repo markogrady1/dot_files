1e03a89299e9e3589c562f3cdebb951b85206158
Mark O Grady <mark.o'grady@broadbean.com>
2021-02-16 13:38:27 +0000

    add the cloning of vundle

--- install_env.sh
+++ install_env.sh
@@ -10,16 +10,17 @@
 #fi
 
 cp dot_files/perltidy/perltidyrc.symlink ~/.perltidyrc
+
 cat dot_files/vim/vimrc.symlink >> ~/.vimrc
 mkdir -p ~/.vim/colors
 cp dot_files/vim/colors/dracula.vim ~/.vim/colors/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 if [ ! -f "~/.bashrc" ]; then
     touch ~/.bashrc
 fi
 
 cat dot_files/bash/PS1.snippet >> ~/.bashrc
-
 cp dot_files/bash/aliases.symlink ~/.bash_aliases
 
 echo 'source ~/.bash_aliases' >> ~/.bashrc
