e17a64c430d3d7a878cdc680f2239a674e56d6c8
Mark O Grady <mark.o'grady@broadbean.com>
2021-10-10 21:20:36 +0100

    update install script

--- install_env.sh
+++ install_env.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp dot_files/perltidy/perltidyrc.symlink ~/.perltidyrc
+if [[ $OSTYPE == *linux* ]]
+then
+    echo "Hello Linux!"
 
-cat dot_files/vim/vimrc.symlink >> ~/.vimrc
+elif [[ $OSTYPE == *darwin* ]]
+then
+    echo "Hello MacOS!"
+    xcode-select --install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install zsh
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    # oh-my-zsh theme
+    cp zsh/themes/mark-amuse.zsh-theme ~/.oh-my-zsh/themes/
+    sed -i "s+robbyrussell+mark-amuse+" ~/.zshrc
+
+fi
+
+cat vim/vimrc.symlink >> ~/.vimrc
 mkdir -p ~/.vim/colors
-cp dot_files/vim/colors/dracula.vim ~/.vim/colors/
-cp dot_files/vim/templates/pm_header.temp ~/.vim/pm_header.temp
-cp dot_files/vim/templates/pl_header.temp ~/.vim/pl_header.temp
+cp vim/colors/dracula.vim ~/.vim/colors/
+cp vim/templates/pm_header.temp ~/.vim/pm_header.temp
+cp vim/templates/pl_header.temp ~/.vim/pl_header.temp
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -15,11 +30,15 @@ if [ ! -f "~/.bashrc" ]; then
     touch ~/.bashrc
 fi
 
-cat dot_files/bash/PS1.snippet >> ~/.bashrc
-cp dot_files/bash/aliases.symlink ~/.bash_aliases
+cat bash/PS1.snippet >> ~/.bashrc
+cp bash/aliases.symlink ~/.bash_aliases
 
 echo 'source ~/.bash_aliases' >> ~/.bashrc
 
 # Git configuration
-cp dot_files/git/gitconfig.symlink ~/.gitconfig
-cp dot_files/git/githelpers.symlink ~/.githelpers
+cp git/gitconfig.symlink ~/.gitconfig
+cp git/githelpers.symlink ~/.githelpers
+cp perltidy/perltidyrc.symlink ~/.perltidyrc
+
+
+# open vimrc and run :PluginInstall
